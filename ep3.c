56521bbe02441b39f8f75158323063aa7bdd2514
heitorc62 <heitor.c@usp.br>
2022-06-25 14:52:55 -0300

    Implementamos as funções de aproximação de integral (integração numérica) utilizando os métodos do trapézio e o método de simpson.

--- ep3.c
+++ ep3.c
@@ -26,6 +26,7 @@ double* valsInterpolados(int totalPontosConhecidos, int totalPontosInterpolados,
         for(int auxJ = 0; auxJ < totalPontosConhecidos; auxJ++){
             valor += pontosY[auxJ] * lPontos[auxJ + (j * totalPontosConhecidos)];
         }
+        //printf("%f\n", valPontos[j]);
         valPontos[j] = valor;  
     }
     return valPontos;
@@ -35,22 +36,47 @@ double* valsInterpolados(int totalPontosConhecidos, int totalPontosInterpolados,
 
 double* lagrange(int totalPontosConhecidos, double pontosX[], double pontosY[], int totalPontosInterpolados, double xInterpolados[]){
     double *lPontos = valoresL(totalPontosConhecidos, pontosX, totalPontosInterpolados, xInterpolados);
-    double *valorPontos = valsInterpolado(totalPontosConhecidos, totalPontosInterpolados, pontosY, lPontos);
+    double *valorPontos = valsInterpolados(totalPontosConhecidos, totalPontosInterpolados, pontosY, lPontos);
     free(lPontos);
     return valorPontos;
 }
 
+double* interpolaY(double valMax, double h, int totalPontosConhecidos, double pontosX[], double pontosY[]){
+    double* pontosInterpolados;
+    pontosInterpolados = (double*) malloc(((valMax/h) + 1) * sizeof(double));
+    for(int i = 0; i < (int)((valMax/h) + 1); i++) pontosInterpolados[i] = i*h;
+    double* yInterpolados = lagrange(totalPontosConhecidos, pontosX, pontosY, (valMax/h) + 1, pontosInterpolados);
+    return yInterpolados;
+}
+
+
+double trapezio(double valMax, double h, double yInterpolados[]){
+    double area = 0;
+    for(int i = 0; i < (int)((valMax/h)); i++)
+        area += yInterpolados[i] + yInterpolados[i+1];
+    return (h/2)*area;
+}
+
+double simpson(double valMax, double h, double yInterpolados[]){
+    double area = 0;
+    for(int i = 0; i < (int)((valMax/h) - 1); i += 2)
+        area += yInterpolados[i] + (4*yInterpolados[i+1]) + yInterpolados[i+2];
+    return (h/3)*area;
+}
+
 
 int main(){
-    double pontoX[7] = {0, 5, 10, 15, 20, 25, 30};
-    double pontY[7] = {0, 1.5297, 9.5120, 8.7025, 2.8087, 1.0881, 0.3537};
+    double pontosX[7] = {0, 5, 10, 15, 20, 25, 30};
+    double pontosY[7] = {0, 1.5297, 9.5120, 8.7025, 2.8087, 1.0881, 0.3537};
+    double h = 1;
+    int totalPontos = sizeof(pontosX) / sizeof(pontosX[0]);
+    double valMax = pontosX[totalPontos - 1];
+    double* yInterpolados = interpolaY(valMax, h, totalPontos, pontosX, pontosY);
+    double area1 = trapezio(valMax, h, yInterpolados);
+    double area2 = simpson(valMax, h, yInterpolados);
+    printf("Integral trapezio = %f\n", area1);
+    printf("Integral simpson = %f\n", area2);
 
-
-
-
-
-
-
-
+    
     return 0;
 }
